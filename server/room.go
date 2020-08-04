a5958b7b786d3ba0024f980a1f473223b6824215
武新飞 <xinfei.wu@qq.com>
2020-08-04 11:24:56 +0800

    api: implements room api

--- server/room.go
+++ server/room.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gocraft/dbr/v2"
 	"jhmeeting.com/adminserver/app"
+	"jhmeeting.com/adminserver/db"
 )
 
 type RoomServer struct {
@@ -16,25 +21,92 @@ func NewRoomServer(app *app.App) *RoomServer {
 }
 
 func (s RoomServer) Info(c *gin.Context) {
-
+	var param struct {
+		ID int64
+	}
+	if c.BindJSON(&param) != nil {
+		return
+	}
+	uid := c.GetInt64("uid")
+	room := app.RoomInfo{}
+	err := s.DB().Select("*").From("room").
+		Where("id=? and uid=?", param.ID, uid).LoadOneContext(c, &room)
+	if err != nil {
+		c.AbortWithError(http.StatusNotFound, err)
+		return
+	}
+	c.JSON(200, room)
 }
 
 func (s RoomServer) Create(c *gin.Context) {
+	roomInfo := app.RoomInfo{}
+	if c.BindJSON(&roomInfo) != nil {
+		return
+	}
 
+	roomInfo.Uid = c.GetInt64("uid")
+	roomInfo.Ctime = time.Now()
+
+	_, err := s.DB().InsertInto("room").
+		Columns("uid", "room_name", "allow_anonymous", "config", "ctime").
+		Record(&roomInfo).ExecContext(c)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	c.JSON(200, gin.H{
+		"id": roomInfo.Id,
+	})
 }
 
 func (s RoomServer) Delete(c *gin.Context) {
-
+	var param struct {
+		ID int64
+	}
+	if c.BindJSON(&param) != nil {
+		return
+	}
+	uid := c.GetInt64("uid")
+	_, err := s.DB().DeleteFrom("room").Where("id=? and uid=?", param.ID, uid).ExecContext(c)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
 }
 
 func (s RoomServer) Modify(c *gin.Context) {
-
+	roomInfo := app.RoomInfo{}
+	if c.BindJSON(&roomInfo) != nil {
+		return
+	}
+	uid := c.GetInt64("uid")
+	_, err := s.DB().Update("room").
+		Set("participant_limits", roomInfo.ParticipantLimits).
+		Set("allow_anonymous", roomInfo.AllowAnonymous).
+		Set("config", roomInfo.Config).
+		Where("id=? and uid=?", roomInfo.Id, uid).
+		ExecContext(c)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
 }
 
 func (s RoomServer) List(c *gin.Context) {
+	var param db.Pagination
+	if c.BindJSON(&param) != nil {
+		return
+	}
 
+	uid := c.GetInt64("uid")
+	rooms := []app.RoomInfo{}
+
+	result, _ := db.NewSelector(s.DB()).From("room").
+		Where(dbr.Eq("uid", uid)).
+		Paginate(param.Page, param.PerPage).LoadPage(&rooms)
+	c.JSON(200, result)
 }
 
 func (s RoomServer) Token(c *gin.Context) {
-
+	s.APIRoute(c, "/api/conference/token")
 }
