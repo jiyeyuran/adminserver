1ae57a039589bbc8f4c621c1118a0de9c5ce78ae
武新飞 <xinfei.wu@qq.com>
2020-08-07 13:20:23 +0800

    check https port

--- main.go
+++ main.go
@@ -1,41 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "github.com/unrolled/secure"
+	"fmt"
 
-    "github.com/gin-gonic/gin"
-    "jhmeeting.com/adminserver/app"
-    "jhmeeting.com/adminserver/routes"
+	"github.com/unrolled/secure"
+
+	"github.com/gin-gonic/gin"
+	"jhmeeting.com/adminserver/app"
+	"jhmeeting.com/adminserver/routes"
 )
 
 func main() {
-    r := gin.Default()
-    https := gin.Default()
-    app := app.NewApp()
+	r := gin.Default()
+	https := gin.Default()
+	app := app.NewApp()
 
-    httpsPort := fmt.Sprintf(":%d", app.Config().HttpsPort)
-    https.Use(TlsHandler(httpsPort))
+	if app.Config().HttpsPort > 0 {
+		httpsPort := fmt.Sprintf(":%d", app.Config().HttpsPort)
+		https.Use(TlsHandler(httpsPort))
+		routes.Setup(https, app)
+		go https.RunTLS(httpsPort, app.Config().CertPath, app.Config().KeyPath)
+	}
 
-    routes.Setup(r, app)
-    routes.Setup(https, app)
+	routes.Setup(r, app)
 
-    go https.RunTLS(httpsPort, app.Config().CertPath, app.Config().KeyPath)
-
-    r.Run(fmt.Sprintf(":%d", app.Config().Port))
+	r.Run(fmt.Sprintf(":%d", app.Config().Port))
 }
 
 // 初始 TLS
 func TlsHandler(httpsPort string) gin.HandlerFunc {
-    return func(c *gin.Context) {
-        secureMiddleware := secure.New(secure.Options{
-            SSLRedirect: true,
-            SSLHost:     httpsPort,
-        })
-        err := secureMiddleware.Process(c.Writer, c.Request)
-        if err != nil {
-            return
-        }
-        c.Next()
-    }
+	return func(c *gin.Context) {
+		secureMiddleware := secure.New(secure.Options{
+			SSLRedirect: true,
+			SSLHost:     httpsPort,
+		})
+		err := secureMiddleware.Process(c.Writer, c.Request)
+		if err != nil {
+			return
+		}
+		c.Next()
+	}
 }
