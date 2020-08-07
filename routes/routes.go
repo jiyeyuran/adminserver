69ab46a0a92cac5a4e66575424ad32a7f6b6178e
武新飞 <xinfei.wu@qq.com>
2020-08-07 14:05:11 +0800

    serve www at /admin

--- routes/routes.go
+++ routes/routes.go
@@ -1,74 +1,75 @@
 package routes
 
 import (
-    "github.com/gin-contrib/static"
-    "net/http"
-    "time"
+	"net/http"
+	"time"
 
-    "github.com/dchest/captcha"
-    "github.com/gin-gonic/gin"
-    "jhmeeting.com/adminserver/app"
-    "jhmeeting.com/adminserver/server"
+	"github.com/gin-contrib/static"
+
+	"github.com/dchest/captcha"
+	"github.com/gin-gonic/gin"
+	"jhmeeting.com/adminserver/app"
+	"jhmeeting.com/adminserver/server"
 )
 
 func Setup(r *gin.Engine, app *app.App) {
-    gin.SetMode(gin.DebugMode)
+	gin.SetMode(gin.DebugMode)
 
-    r.Use(static.Serve("/", static.LocalFile("./www", true)))
+	r.Use(static.Serve("/admin", static.LocalFile("./www", true)))
 
-    admin := r.Group("/admin")
-    admin.Use(errorMiddleware, timeoutMiddleware(5*time.Second))
-    {
+	admin := r.Group("/admin")
+	admin.Use(errorMiddleware, timeoutMiddleware(5*time.Second))
+	{
 
-        admin.POST("/captcha-id", handleCaptchaId)
-        admin.GET("/captcha-id", handleCaptchaId)
-        admin.GET("/captcha/:id", gin.WrapH(captcha.Server(captcha.StdWidth, captcha.StdHeight)))
+		admin.POST("/captcha-id", handleCaptchaId)
+		admin.GET("/captcha-id", handleCaptchaId)
+		admin.GET("/captcha/:id", gin.WrapH(captcha.Server(captcha.StdWidth, captcha.StdHeight)))
 
-        passport := admin.Group("/passport")
-        {
-            server := server.NewPassportServer(app)
-            passport.POST("/signup", server.Signup)
-            passport.POST("/login", server.Login)
-            passport.POST("/logout", server.Logout)
-            passport.POST("/info", authMiddleware(app), server.Info)
-            passport.POST("/modify", authMiddleware(app), server.Modify)
-        }
+		passport := admin.Group("/passport")
+		{
+			server := server.NewPassportServer(app)
+			passport.POST("/signup", server.Signup)
+			passport.POST("/login", server.Login)
+			passport.POST("/logout", server.Logout)
+			passport.POST("/info", authMiddleware(app), server.Info)
+			passport.POST("/modify", authMiddleware(app), server.Modify)
+		}
 
-        roomGroup := admin.Group("/room", authMiddleware(app))
-        {
-            roomServer := server.NewRoomServer(app)
-            roomGroup.POST("/info", roomServer.Info)
-            roomGroup.POST("/list", roomServer.List)
-            roomGroup.POST("/create", roomServer.Create)
-            roomGroup.POST("/modify", roomServer.Modify)
-            roomGroup.POST("/delete", roomServer.Delete)
-            roomGroup.POST("/token", roomServer.Token)
-        }
+		roomGroup := admin.Group("/room", authMiddleware(app))
+		{
+			roomServer := server.NewRoomServer(app)
+			roomGroup.POST("/info", roomServer.Info)
+			roomGroup.POST("/list", roomServer.List)
+			roomGroup.POST("/create", roomServer.Create)
+			roomGroup.POST("/modify", roomServer.Modify)
+			roomGroup.POST("/delete", roomServer.Delete)
+			roomGroup.POST("/token", roomServer.Token)
+		}
 
-        conferenceGroup := admin.Group("/conference", authMiddleware(app))
-        {
-            conferenceServer := server.NewConferenceServer(app)
-            conferenceGroup.POST("/info", conferenceServer.Info)
-            conferenceGroup.POST("/runing", conferenceServer.Runing)
-            conferenceGroup.POST("/dispose", conferenceServer.Dispose)
-            conferenceGroup.POST("/lock", conferenceServer.Lock)
-            conferenceGroup.POST("/unlock", conferenceServer.Unlock)
-            conferenceGroup.POST("/history", conferenceServer.History)
-            conferenceGroup.POST("/action", conferenceServer.Action)
-        }
+		conferenceGroup := admin.Group("/conference", authMiddleware(app))
+		{
+			conferenceServer := server.NewConferenceServer(app)
+			conferenceGroup.POST("/info", conferenceServer.Info)
+			conferenceGroup.POST("/runing", conferenceServer.Runing)
+			conferenceGroup.POST("/dispose", conferenceServer.Dispose)
+			conferenceGroup.POST("/lock", conferenceServer.Lock)
+			conferenceGroup.POST("/unlock", conferenceServer.Unlock)
+			conferenceGroup.POST("/history", conferenceServer.History)
+			conferenceGroup.POST("/action", conferenceServer.Action)
+		}
 
-        recordGroup := admin.Group("/record", authMiddleware(app))
-        {
-            recordServer := server.NewRecordServer(app)
-            recordGroup.POST("/info", recordServer.Info)
-            recordGroup.POST("/list", recordServer.List)
-            recordGroup.POST("/delete", recordServer.Delete)
-        }
-    }
+		recordGroup := admin.Group("/record", authMiddleware(app))
+		{
+			recordServer := server.NewRecordServer(app)
+			recordGroup.POST("/info", recordServer.Info)
+			recordGroup.POST("/list", recordServer.List)
+			recordGroup.POST("/delete", recordServer.Delete)
+		}
+	}
 }
 
 func handleCaptchaId(c *gin.Context) {
-    c.AbortWithStatusJSON(http.StatusOK, gin.H{
-        "id": captcha.NewLen(6),
-    })
+	c.AbortWithStatusJSON(http.StatusOK, gin.H{
+		"id": captcha.NewLen(6),
+	})
 }
